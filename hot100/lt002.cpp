80014d89f20dbef9da42cd6588a4ed59882052d2
Rising_Date <1773261467@qq.com>
2026-01-26 21:25:19 +0800

    init

---
+++ hot100/lt002.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Vier on 26-1-26.
+//
+class Solution {
+public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans;
+
+
+        return ans;
+    }
+};
+
+int main() {
+
+}
