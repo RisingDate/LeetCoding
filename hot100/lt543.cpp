9c0b7b66310509ba0b3ea566536982ab1fd8cd10
Rising_Date <1773261467@qq.com>
2026-01-30 23:24:05 +0800

    增加543

---
+++ hot100/lt543.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Vier on 26-1-30.
+//
+//
+// Created by Vier on 26-1-30.
+//
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int ans=0;
+    int dfs(TreeNode* x) {
+        if(!x) return -1;
+        int l = dfs(x->left)+1;
+        int r = dfs(x->right)+1;
+        ans = max(ans, l+r);
+        return max(l, r);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
