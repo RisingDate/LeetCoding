300e96fc752ba13828d4ed45c3b78a61cff932ef
Rising_Date <1773261467@qq.com>
2026-01-30 10:35:29 +0800

    增加019

---
+++ hot100/lt019.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 17732 on 26-1-30.
+//
+#include<bits/stdc++.h>
+using namespace	std;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		ListNode* preHead = new ListNode(0, head);
+		ListNode* p = preHead;
+		ListNode* q = preHead;
+		for (int i=0; i<n; i++) q=q->next;
+		while (q->next)
+			p=p->next, q=q->next;
+		p->next=p->next->next;
+		return preHead->next;
+	}
+};
