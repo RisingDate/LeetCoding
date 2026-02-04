e3595a211a067f8d281922f9b65431af441a4a49
Rising_Date <1773261467@qq.com>
2026-02-04 21:28:42 +0800

    增加064

---
+++ hot100/lt064.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 17732 on 26-2-4.
+//
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+	int minPathSum(vector<vector<int>>& grid) {
+		int m = grid.size();
+		int n = grid[0].size();
+		vector<vector<int>> dp(m, vector<int>(n, 0));
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i==0 && j==0) dp[i][j] = grid[i][j];
+				else if (i == 0) dp[i][j] = dp[i][j-1] + grid[i][j];
+				else if (j == 0) dp[i][j] = dp[i-1][j] + grid[i][j];
+				else dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+};
